dce4ddd450d7eb86b52c5c14721e4df4cdf95519
= <=>
2022-11-08 20:45:30 -0300

    Atualização

--- salario.js
+++ salario.js
@@ -9,5 +9,17 @@ let remuneracao = Number(read.question("Digite a Remuneracao pretendida: ").repl
 
 let reservaremuneracao = (remuneracao - (remuneracao * 70) / 100)
 let reservainvestimento = (remuneracao - (remuneracao * 80) / 100)
+
+console.log("-----------------------------")
 console.log (`A Reserva da Remuneracao  = ${reservaremuneracao.toFixed(2)}`)
 console.log (`A Reserva para Investimentos  = ${reservainvestimento.toFixed(2)}`)
+console.log("-----------------------------")
+let custooperacional = Number(read.question("Digite o Custo Operacional: ").replace(",","."))
+let cargahoraria = Number(read.question("Digite a Carga Horaria: ").replace(",","."))
+
+let horatecnica = (remuneracao +(reservaremuneracao +(reservainvestimento + custooperacional))) / cargahoraria
+console.log("-----------------------------")
+console.log (`A Reserva da Remuneracao  = ${reservaremuneracao.toFixed(2)}`)
+console.log (`A Reserva para Investimentos  = ${reservainvestimento.toFixed(2)}`)
+console.log (`A Hora Tecnica = ${horatecnica.toFixed(2)}`)
+console.log("-----------------------------")
